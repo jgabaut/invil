63a2307d8f9f288e4cb6078529e5d52a08fef4b4
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-02-18 20:51:02 +0100

    chore: add warn on try_lex_makefile()
    feat: parse build-system requirements array
    
    chore: parse build_backend before printing it, log anvilpy_env
    
    feat: parse and log authors array
    
    feat: collect urls
    
    feat: parse classifiers
    
    feat: collect entrypoints

--- src/anvil_py.rs
+++ src/anvil_py.rs
@@ -10,6 +10,10 @@ pub const ANVILPY_PROJECT_NAME_KEYNAME: &str = "name";
 pub const ANVILPY_PROJECT_DESC_KEYNAME: &str = "description";
 pub const ANVILPY_PROJECT_README_KEYNAME: &str = "readme";
 pub const ANVILPY_PROJECT_PYTHONV_REQ_KEYNAME: &str = "requires-python";
+pub const ANVILPY_PROJECT_CLASSIFIERS_KEYNAME: &str = "classifiers";
+pub const ANVILPY_PROJECT_AUTHORS_KEYNAME: &str = "authors";
+pub const ANVILPY_PROJECT_URLS_KEYNAME: &str = "urls";
+pub const ANVILPY_PROJECT_ENTRYPOINTS_KEYNAME: &str = "scripts";
 pub const ANVILPY_BUILD_REQS_KEYNAME: &str = "requires";
 pub const ANVILPY_BUILD_BACKEND_KEYNAME: &str = "build-backend";
 
@@ -27,6 +31,14 @@ pub struct UrlEntry {
     pub name: String,
     /// Url anchor
     pub link: Url,
+}
+
+#[derive(Debug)]
+pub struct ScriptEntry {
+    /// Script name
+    pub name: String,
+    /// Script entrypoint path
+    pub entrypoint: String,
 }
 
 #[derive(Debug)]
@@ -62,10 +74,10 @@ pub struct AnvilPyEnv {
     pub classifiers: Vec<String>,
 
     /// Entrypoints
-    pub scripts: Option<Vec<String>>,
+    pub scripts: Vec<ScriptEntry>,
 
     /// Project links
-    pub urls: Option<Vec<UrlEntry>>,
+    pub urls: Vec<UrlEntry>,
 
     /// Build system
     pub build_sys: BuildSystem,
@@ -91,8 +103,8 @@ pub fn parse_pyproject_toml(pyproj_path: &PathBuf) -> Result<AnvilPyEnv,String> 
                 python_version_req: "".to_string(),
                 readme_path: PathBuf::from(""),
                 classifiers: Vec::<String>::new(),
-                scripts: Some(Vec::<String>::new()),
-                urls: Some(Vec::<UrlEntry>::new()),
+                scripts: Vec::<ScriptEntry>::new(),
+                urls: Vec::<UrlEntry>::new(),
                 build_sys: BuildSystem { reqs: Vec::<String>::new(), backend: "".to_string() },
             };
             trace!("Toml value: {{{}}}", y);
@@ -116,7 +128,6 @@ pub fn parse_pyproject_toml(pyproj_path: &PathBuf) -> Result<AnvilPyEnv,String> 
                     let anvilpy_proj_name_str = proj_name.as_str().expect("toml conversion failed");
                     debug!("anvilpy_name: {{{anvilpy_proj_name_str}}}");
                     anvilpy_env.proj_name = anvilpy_proj_name_str.to_string();
-
                 } else {
                     debug!("Missing ANVILPY_PROJECT_NAME definition.");
                 }
@@ -124,7 +135,6 @@ pub fn parse_pyproject_toml(pyproj_path: &PathBuf) -> Result<AnvilPyEnv,String> 
                     let anvilpy_proj_desc_str = proj_desc.as_str().expect("toml conversion failed");
                     debug!("anvilpy_desc: {{{anvilpy_proj_desc_str}}}");
                     anvilpy_env.description = anvilpy_proj_desc_str.to_string();
-
                 } else {
                     debug!("Missing ANVILPY_PROJECT_DESC definition.");
                 }
@@ -132,7 +142,6 @@ pub fn parse_pyproject_toml(pyproj_path: &PathBuf) -> Result<AnvilPyEnv,String> 
                     let anvilpy_proj_readme_str = proj_readme.as_str().expect("toml conversion failed");
                     debug!("anvilpy_readme: {{{anvilpy_proj_readme_str}}}");
                     anvilpy_env.readme_path = PathBuf::from(anvilpy_proj_readme_str.to_string());
-
                 } else {
                     debug!("Missing ANVILPY_PROJECT_DESC definition.");
                 }
@@ -143,21 +152,119 @@ pub fn parse_pyproject_toml(pyproj_path: &PathBuf) -> Result<AnvilPyEnv,String> 
                 } else {
                     debug!("Missing ANVILPY_PROJECT_PYVERSION_REQ definition.");
                 }
+                if let Some(authors_val) = project_table.get(ANVILPY_PROJECT_AUTHORS_KEYNAME) {
+                    if let Some(authors_array) = authors_val.as_array() {
+                        for author in authors_array {
+                            if let Some(author_table) = author.as_table() {
+                                let name = author_table.get("name").and_then(|v| v.as_str()).expect("toml conversion failed");
+                                let email = author_table.get("email").and_then(|v| v.as_str()).expect("toml conversion failed");
+                                let auth = Author { name: name.to_string(), email: Some(email.to_string())};
+                                debug!("anvilpy_author: {{{:?}}}", auth);
+                                anvilpy_env.authors.push(auth);
+                            }
+                        }
+                    } else {
+                        error!("ANVILPY_PROJECT_AUTHORS is not an array");
+                        return Err("Authors is not an array".to_string());
+                    }
+                } else {
+                    warn!("Missing ANVILPY_AUTHORS definition.");
+                }
+                if let Some(classifiers_val) = project_table.get(ANVILPY_PROJECT_CLASSIFIERS_KEYNAME) {
+                    if let Some(classifiers_array) = classifiers_val.as_array() {
+                        for cls in classifiers_array {
+                            let cls_str = cls.as_str().expect("toml conversion failed");
+                            debug!("anvilpy_classifier: {{{}}}", cls_str);
+                            anvilpy_env.classifiers.push(cls_str.to_string());
+                        }
+                    } else {
+                        error!("ANVILPY_PROJECT_CLASSIFIERS is not an array");
+                        return Err("Classifiers is not an array".to_string());
+                    }
+                } else {
+                    warn!("Missing ANVILPY_CLASSIFIERS definition.");
+                }
+                if let Some(urls_val) = project_table.get(ANVILPY_PROJECT_URLS_KEYNAME) {
+                    if let Some(urls_table) = urls_val.as_table() {
+                        for (k, v) in urls_table {
+                            if let Some(url) = v.as_str() {
+                                let url_with_protocol = if url::Url::parse(url).is_ok() {
+                                    url.to_string()
+                                } else {
+                                    trace!("{{{url}}} was not a valid url, trying to prepend https protocol");
+                                    format!("https://{}", url)
+                                };
+
+                                debug!("anvilpy_project_url: {{{}}}", url_with_protocol);
+                                debug!("anvilpy_project_url_name: {{{}}}", k);
+                                let url_parsed;
+                                match Url::parse(&url_with_protocol) {
+                                    Ok(u) => {
+                                        url_parsed = u;
+                                    }
+                                    Err(e) => {
+                                        error!("Failed parsing url: {{{}}}. Err was: {e}", url);
+                                        return Err("Failed parsing url".to_string());
+                                    }
+                                }
+                                let url_entry = UrlEntry { name: k.to_string(), link: url_parsed };
+                                anvilpy_env.urls.push(url_entry);
+                            }
+                        }
+                    } else {
+                        error!("ANVILPY_URLS is not a table");
+                        return Err("Urls is not an table".to_string());
+                    }
+                } else {
+                    warn!("Missing ANVILPY_URLS definition.");
+                }
+                if let Some(entrypoints_val) = project_table.get(ANVILPY_PROJECT_ENTRYPOINTS_KEYNAME) {
+                    if let Some(entrypoints_table) = entrypoints_val.as_table() {
+                        for (k, v) in entrypoints_table {
+                            if let Some(entry_path) = v.as_str() {
+                                debug!("anvilpy_project_entrypoint_path: {{{}}}", entry_path);
+                                debug!("anvilpy_project_entrypoint_name: {{{}}}", k);
+                                let script_entry = ScriptEntry { name: k.to_string(), entrypoint: entry_path.to_string()};
+                                anvilpy_env.scripts.push(script_entry);
+                            }
+                        }
+                    } else {
+                        error!("ANVILPY_ENTRYPOINTS is not a table");
+                        return Err("Scripts is not an table".to_string());
+                    }
+                } else {
+                    warn!("Missing ANVILPY_ENTRYPOINTS definition.");
+                }
             } else {
                 error!("Missing project section.");
                 return Err(format!("Missing project section in {{{}}}", pyproj_path.display()));
             }
             if let Some(build_system_table) = y.get("build-system").and_then(|v| v.as_table()) {
                 if let Some(reqs) = build_system_table.get(ANVILPY_BUILD_REQS_KEYNAME) {
-                    debug!("TODO: parse reqs array");
-                    trace!("ANVILPY_BUILD_REQS: {{{reqs}}}");
-                    //anvilpy_env.ba = Some(format!("{}", source_name.as_str().expect("toml conversion failed")));
+                    if let Some(reqs_arr) = reqs.as_array() {
+                        for item in reqs_arr {
+                            match item {
+                                toml::Value::String(str_v) => {
+                                    trace!("ANVILPY_BUILD_REQS: {{{str_v}}}");
+                                    anvilpy_env.build_sys.reqs.push(str_v.to_string());
+                                }
+                                _ => {
+                                    error!("Unexpected item for reqs arrays: {{{}}}", item);
+                                    return Err("Unexpected item in reqs array".to_string());
+                                }
+                            }
+                        }
+                    } else {
+                        error!("ANVILPY_BUILD_REQS is not an array");
+                        return Err("Reqs is not an array".to_string());
+                    }
                 } else {
                     warn!("Missing ANVILPY_BUILD_REQS definition.");
                 }
                 if let Some(build_backend) = build_system_table.get(ANVILPY_BUILD_BACKEND_KEYNAME) {
-                    trace!("ANVILPY_BUILD_BACKEND: {{{build_backend}}}");
-                    //anvil_env.bin = Some(format!("{}", binary_name.as_str().expect("toml conversion failed")));
+                    let backend_str = build_backend.as_str().expect("toml conversion failed");
+                    trace!("ANVILPY_BUILD_BACKEND: {{{backend_str}}}");
+                    anvilpy_env.build_sys.backend = backend_str.to_string();
                 } else {
                     warn!("Missing ANVILPY_BUILD_BACKEND definition.");
                 }
